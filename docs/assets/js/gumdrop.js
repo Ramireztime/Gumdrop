6c671ab6f33bfa64635674ec323d19b7e5280fc0
Angel Ramirez <Angel Ramirez>
2016-09-21 22:29:24 -0700

    more updates to docs

--- docs/assets/js/gumdrop.js
+++ docs/assets/js/gumdrop.js
@@ -6,16 +6,3 @@ function toggle_visibility(id) {
     else
         e.style.display = 'block';
 }
-
-// Close alerts *deprecated* jquery is no longer included by default
-$("document").ready(function(){
-	$("#hide").click(function(){	
-		$("#theDiv").hide("fast");
-	});
-	$("#show").click(function(){	
-		$("#theDiv").show("normal");
-	});
-	$("#toggle").click(function(){
-		$("#theDiv").toggle(500, function(){null});
-	});		
-});
